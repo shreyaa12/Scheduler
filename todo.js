0b6a763e9d54823eada742e8ff92845506f41069
shreyaa12 <shreyagokani31@gmail.com>
2023-03-07 13:32:15 -0700

    with best ES6 Practises

--- todo.js
+++ todo.js
@@ -1,96 +1,68 @@
-var todoList = document.getElementById("list");
-var newItem = document.getElementById("name");
-var todoType = document.getElementById("todoType");
-var table = document.createElement("table");
-var tbody = document.createElement("tbody");
+const todoList = document.getElementById("list");
+const newItem = document.getElementById("name");
+const todoType = document.getElementById("todoType");
+const tbody = document.createElement("tbody");
+const categories = {
+  Home: "#ffc107",
+  Work: "#28a745",
+  Hobby: "#17a2b8",
+  Urgent: "#dc3545",
+};
 todoList.appendChild(tbody);
 
-function addItem() {
-	todoList.style.visibility = "visible";
-  var taskText = newItem.value.trim();
-  var todoVal = todoType.value;
-  if (taskText === "") {
-    alert("Please enter a task.");
-    return;
-  } else if (todoVal === "") {
-    alert("Please enter a task type.");
+let addItem= function(){
+ todoList.style.visibility = "visible";
+  const taskText = newItem.value.trim();
+  let todoVal = todoType.value;
+  if (!taskText || !todoVal) {
+    alert("Please enter a task and a task type.");
     return;
   }
-  var tr = document.createElement("tr");
-  var taskTd = document.createElement("td");
-  var task = document.createTextNode(newItem.value);
-  var deleteBtnTd = document.createElement("td");
-  var deleteBtn = document.createElement("button");
+  const tr = document.createElement("tr");
+  const taskTd = document.createElement("td");
+  const task = document.createTextNode(taskText);
+  const deleteBtnTd = document.createElement("td");
+  const deleteBtn = document.createElement("button");
   deleteBtn.className = "delete-button";
-  var editBtnTd = document.createElement("td");
-  var editBtn = document.createElement("button");
+  const editBtnTd = document.createElement("td");
+  const editBtn = document.createElement("button");
   editBtn.className = "glyphicon glyphicon-pencil";
-  var todoTypeTd = document.createElement("td");
-  var todoTypeSpan = document.createElement("span");
+  const todoTypeTd = document.createElement("td");
+  const todoTypeSpan = document.createElement("span");
   todoTypeSpan.className = "category";
-  todoTypeSpan.appendChild(document.createTextNode(todoVal));
-  switch (todoVal) {
-    case "Home":
-      tr.style.backgroundColor = "#ffc107"; //yellow
-      break;
-    case "Work":
-      tr.style.backgroundColor = "#28a745"; // green
-      break;
-    case "Hobby":
-      tr.style.backgroundColor = "#17a2b8"; // blue
-      break;
-    case "Urgent":
-      tr.style.backgroundColor = "#dc3545"; // red
-      break;
-    default:
-      tr.style.backgroundColor = "#fff";
-  }
-
+  todoTypeSpan.textContent = todoVal;
+  const categoryColor = categories[todoVal] || "#fff";
+  tr.style.backgroundColor = categoryColor;
   deleteBtn.innerHTML = "X";
-  /* editBtn.innerHTML = "Edit" */;
   deleteBtn.onclick = function () {
-    var confirmDlt = confirm(" Are you sure you want to delete the task?");
+    const confirmDlt = confirm("Are you sure you want to delete the task?");
     if (confirmDlt) {
       tbody.removeChild(tr);
     }
   };
   editBtn.onclick = function () {
-    var editText = prompt("Edit task or Edit Type Task:", task.nodeValue + "," + todoVal);
-    if (editText !== null && editText !== "") {
-      var taskAndType = editText.split(",");
-      task.nodeValue = taskAndType[0].trim();
-      todoTypeSpan.innerHTML = taskAndType[1].trim();
-      todoVal = taskAndType[1].trim();
-      switch (taskAndType[1].trim()) {
-        case "Home":
-          tr.style.backgroundColor = "#ffc107"; //yellow
-          break;
-        case "Work":
-          tr.style.backgroundColor = "#28a745"; // green
-          break;
-        case "Hobby":
-          tr.style.backgroundColor = "#17a2b8"; // blue
-          break;
-        case "Urgent" || "urgent":
-          tr.style.backgroundColor = "#dc3545"; // red
-          break;
-        default:
-          tr.style.backgroundColor = "#fff";
+    const editText = prompt("Edit task or Edit Type Task:", `${task.nodeValue},${todoVal}`);
+    if (editText) {
+      const [editedTask, editedType] = editText.split(",");
+      const isCategoryExists = categories.hasOwnProperty(editedType);
+      if (!isCategoryExists) {
+        alert(`Category '${editedType}' does not exist.`);
+        return;
+      }else{
+        task.nodeValue = editedTask.trim();
+        todoVal = editedType.trim();
+        todoTypeSpan.textContent = editedType.trim();
+        tr.style.backgroundColor = categories[editedType] || "#fff";
       }
+      
     }
   };
-
   taskTd.appendChild(task);
   todoTypeTd.appendChild(todoTypeSpan);
   deleteBtnTd.appendChild(deleteBtn);
   editBtnTd.appendChild(editBtn);
-  tr.appendChild(taskTd);
-  tr.appendChild(todoTypeTd);
-  tr.appendChild(editBtnTd);
-  tr.appendChild(deleteBtnTd);
+  tr.append(taskTd, todoTypeTd, editBtnTd, deleteBtnTd);
   tbody.appendChild(tr);
-
   newItem.value = "";
   todoType.value = "";
-  // todoList.appendChild(table);
 }
